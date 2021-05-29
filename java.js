0cea184cec5300ed030911ad1cce203d9a11d8b2
Alan Fernandes <alanf3878@outlook.com>
2021-05-29 10:51:58 -0300

    trabalho1

---
+++ java.js
@@ -0,0 +1,12 @@
+let nome = "Alan"
+let idade = 24
+let ano = 2021
+
+console.log ("Meu ano de nascimento é", idade - ano)
+
+
+let souhumano = true
+let gato = 1
+let cachorro = 3 
+
+console.log (gato + cachorro)
